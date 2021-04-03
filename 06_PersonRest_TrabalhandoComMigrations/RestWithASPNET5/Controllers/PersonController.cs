1034c17525ca759245984161d413ff1b09e4d56e
natan <natan@DESKTOP-0MHIMNT>
2021-04-02 21:20:40 -0300

    adicionada a aula 6 - Migrations

---
+++ 06_PersonRest_TrabalhandoComMigrations/RestWithASPNET5/Controllers/PersonController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNET5.Model;
+using RestWithASPNET5.Business;
+
+namespace RestWithASPNET5.Controllers
+{
+    //Define a versão da Controller
+    [ApiVersion("1")]
+    [ApiController]
+    //Faz o versionamento na rota da controller
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        //Injeção de dependência do serviço
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personservice)
+        {
+            _logger = logger;
+            //Recebe a injeção de dependência no construtor
+            _personBusiness = personservice;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindById(id);
+
+            if (person == null) return NotFound();
+
+            return Ok(person);
+        }       
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            
+            if (person == null) return BadRequest();
+
+            return Ok(_personBusiness.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+
+            if (person == null) return BadRequest();
+
+            return Ok(_personBusiness.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var person = _personBusiness.FindById(id);
+            if (person == null) return NotFound();
+
+            _personBusiness.Delete(id);  
+            
+            return NoContent();
+        }
+
+
+
+    }
+}
